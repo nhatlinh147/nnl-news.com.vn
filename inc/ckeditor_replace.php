c2ce89174c8b18bd835b0894000fb1aec71f53f7
Uchiha Madara <nhatlinh25831@gmail.com>
2023-02-25 23:54:13 +0700

    nnl-news.com.vn

--- inc/ckeditor_replace.php
+++
@@ -1,8 +0,0 @@
-<!-- Ckeditor full package -->
-<script src="//cdn.ckeditor.com/4.17.1/full/ckeditor.js"></script>
-<script>
-    CKEDITOR.replace('Ckeditor_Content', {
-        filebrowserUploadUrl: '<?php echo General::view_link("tuy-chinh-ckeditor") ?>',
-        filebrowserUploadMethod: 'form'
-    });
-</script>
