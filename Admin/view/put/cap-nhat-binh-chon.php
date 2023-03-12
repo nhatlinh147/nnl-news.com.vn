b8c9a9a402e8e11d49d687f2456372c1a74000b2
nhatlinh147 <nguyennhatlinh1711@gmail.com>
2023-03-12 09:39:17 +0700

    nnl-news.com.vn

--- Admin/view/put/cap-nhat-binh-chon.php
+++ Admin/view/put/cap-nhat-binh-chon.php
@@ -1,5 +1,5 @@
 <?php
-include('D:\xampp\htdocs\nnl-news.com.vn\helpers\path.php');
+include('D:\Xampp\htdocs\nnl-news.com.vn\helpers\path.php');
 Path::path_file_include('Database','Format','General','User_comment');
 $Comment = new Comment();
 $get_method = $Comment->updateVoted();
--- Admin/view/put/lay-thong-tin-san-pham.php
+++ Admin/view/put/lay-thong-tin-san-pham.php
@@ -1,5 +1,5 @@
 <?php
-include('D:\xampp\htdocs\nnl-news.com.vn\helpers\path.php');
+include('D:\Xampp\htdocs\nnl-news.com.vn\helpers\path.php');
 Path::path_file_include('Database','Format','General','User_home');
 $Home = new Home();
 $get_method = $Home->search_product();
